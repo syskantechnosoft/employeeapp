cb4f293f7de97e3e3b1166998164a01a81de0d18
Rajesh Nallusamy <rajesh.kit@gmail.com>
2021-07-29 01:15:01 +0530

    first commit

---
+++ src/main/java/com/revature/controller/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.revature.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+	@GetMapping("/greet")
+	public String welcome() {
+		return "Welcome Rajesh";
+	}
+}
